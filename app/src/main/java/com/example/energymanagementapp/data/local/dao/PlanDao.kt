93118171ca89ec0a1e52606ccc418f4fc57c7816
mMarciusss <matukaitis.martynas@gmail.com>
2026-04-17 12:52:00 +0300

    create PlanDao and PlanRepository

---
+++ app/src/main/java/com/example/energymanagementapp/data/local/dao/PlanDao.kt
@@ -0,0 +1,16 @@
+package com.example.energymanagementapp.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.energymanagementapp.data.local.entities.PlanEntity
+
+@Dao
+interface PlanDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrUpdatePlan(plan: PlanEntity)
+
+    @Query("SELECT * FROM plans WHERE date = :date LIMIT 1")
+    suspend fun getPlanByDate(date: String): PlanEntity?
+}
