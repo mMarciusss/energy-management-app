b3097fe104d0b4a88016493358c8dc1f0cef745c
mMarciusss <matukaitis.martynas@gmail.com>
2026-04-20 13:17:13 +0300

    Create BreakSetupScreen with preview

---
+++ app/src/main/java/com/example/energymanagementapp/ui/screens/BreakSetupScreen.kt
@@ -0,0 +1,68 @@
+package com.example.energymanagementapp.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BreakSetupScreen(
+    activityName: String,
+    breakDuration: Int,
+    onIncrease: () -> Unit,
+    onDecrease: () -> Unit,
+    onConfirm: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(text = activityName)
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+
+            Button(onClick = onDecrease) {
+                Text("-")
+            }
+
+            Spacer(Modifier.width(16.dp))
+
+            Text("Break duration: $breakDuration minutes")
+
+            Spacer(Modifier.width(16.dp))
+
+            Button(onClick = onIncrease) {
+                Text("+")
+            }
+        }
+
+        Spacer(Modifier.width(16.dp))
+
+        Button(onClick = onConfirm) {
+            Text("Confirm break")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BreakSetupScreenPreview() {
+    BreakSetupScreen(
+        activityName = "Gym",
+        breakDuration = 30,
+        onIncrease = {},
+        onDecrease = {},
+        onConfirm = {}
+    )
+}
