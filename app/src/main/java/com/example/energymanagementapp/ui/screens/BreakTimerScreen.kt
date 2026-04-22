daea605bb61a7662cc6e9da2173cb8f9ebf9dcfb
mMarciusss <matukaitis.martynas@gmail.com>
2026-04-22 15:38:09 +0300

    Create working timer in BreakTimerScreen

--- app/src/main/java/com/example/energymanagementapp/ui/screens/BreakTimerScreen.kt
+++ app/src/main/java/com/example/energymanagementapp/ui/screens/BreakTimerScreen.kt
@@ -4,19 +4,40 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import kotlinx.coroutines.delay
 
 @Composable
 fun BreakTimerScreen (
     duration: Int,
     onFinish: () -> Unit
 ) {
+    var timeLeft by remember { mutableStateOf(duration * 60) }
+
+    LaunchedEffect(Unit) {
+        while (timeLeft > 0) {
+            delay(1000)
+            timeLeft--
+        }
+        onFinish()
+    }
+
+    val minutes = timeLeft / 60
+    val seconds = timeLeft % 60
+
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
 
-        Text("Break: $duration min")
+        Text(
+            text = String.format("%02d:%02d", minutes, seconds)
+        )
 
         Button(onClick = onFinish) {
-            Text("Finish")
+            Text("Skip")
         }
     }
 }
