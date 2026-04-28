55c24df4c3f0c95d0705163be003826cd74c9b81
mMarciusss <matukaitis.martynas@gmail.com>
2026-04-29 00:00:51 +0300

    Create LegendItem to showcase the meaning between different states o plan completion in PastDaysScreens

--- app/src/main/java/com/example/energymanagementapp/ui/screens/PastDaysScreen.kt
+++ app/src/main/java/com/example/energymanagementapp/ui/screens/PastDaysScreen.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -73,11 +75,38 @@ fun PastDaysScreen(
                 }
             }
         )
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.padding(horizontal = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            LegendItem(Color.Green, "Completed")
+            LegendItem(Color.Yellow, "Partial")
+            LegendItem(Color.Red, "Not completed")
+        }
+
 
         Spacer(modifier = Modifier.height(16.dp))
-
         Button(onClick = onGoHome) {
             Text("Go Home")
         }
+    }
+}
+
+@Composable
+fun LegendItem(color: Color, text: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(horizontal = 8.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(16.dp)
+                .background(color, shape = CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(text)
     }
 }
